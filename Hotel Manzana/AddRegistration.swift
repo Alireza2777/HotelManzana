666bf7fe4fdf0684f05f52b4b0511d25580ff6b1
Ali <alirezamonfared813@gmail.com>
2023-05-10 08:08:25 -0700

    s1

---
+++ Hotel Manzana/AddRegistration.swift
@@ -0,0 +1,128 @@
+//
+//  AddRegistration.swift
+//  Hotel Manzana
+//
+//  Created by Alireza Karimi on 2023-05-09.
+//
+
+import UIKit
+
+class AddRegistrationTableViewController: UITableViewController {
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    let checkInDateLabelIndexPath = IndexPath(row: 0, section: 1)
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDateLabelIndexPath = IndexPath(row: 2, section: 1)
+    let checkOutDatepickerCellIndexPath = IndexPath(row: 3, section: 1)
+    var isCheckInDatePickerVisible = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerVisible
+        }
+    }
+    var isCheckOutDatePickerVisible = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerVisible
+        }
+    }
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var adultsNumberLabel: UILabel!
+    @IBOutlet weak var adultNumberStepper: UIStepper!
+    @IBOutlet weak var childrenNumberLabel: UILabel!
+    @IBOutlet weak var childrenNumberStepper: UIStepper!
+    
+    
+    @IBAction func doneButtonTapped(_ sender: UIBarButtonItem) {
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        let checkIn = checkInDatePicker.date
+        let checkOut = checkOutDatePicker.date
+        let adults = Int(adultNumberStepper.value)
+        let children = Int(childrenNumberStepper.value)
+        
+        print("Done tapped")
+        print("First Name: \(firstName)")
+        print("Last Name: \(lastName)")
+        print("Email: \(email)")
+        print("Check In: \(checkIn)")
+        print("Check Out: \(checkOut)")
+        print("Number of Adults: \(adults)")
+        print("Number of Children: \(children)")
+    }
+    @IBAction func datePickerValueChanged(_ sender: Any) {
+        updateDateViews()
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        updateNumberOfGuests()
+    }
+    
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = Calendar.current.date(byAdding: .day, value: 1, to: checkInDatePicker.date)
+        
+        checkInDateLabel.text = checkInDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        checkOutDateLabel.text = checkOutDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+    }
+    
+    func updateNumberOfGuests() {
+        adultsNumberLabel.text = "\(Int(adultNumberStepper.value))"
+        childrenNumberLabel.text = "\(Int(childrenNumberStepper.value))"
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath where isCheckInDatePickerVisible == false:
+            return 0
+        case checkOutDatepickerCellIndexPath where isCheckOutDatePickerVisible == false:
+            return 0
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath:
+            return 190
+        case checkOutDatepickerCellIndexPath:
+            return 190
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == checkInDateLabelIndexPath && !isCheckOutDatePickerVisible{
+            isCheckInDatePickerVisible.toggle()
+        } else if indexPath == checkOutDateLabelIndexPath && !isCheckInDatePickerVisible {
+            isCheckOutDatePickerVisible.toggle()
+        } else if indexPath == checkInDateLabelIndexPath || indexPath == checkOutDateLabelIndexPath {
+            isCheckInDatePickerVisible.toggle()
+            isCheckOutDatePickerVisible.toggle()
+        } else {
+            return
+        }
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let startOfDay = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = startOfDay
+        checkInDatePicker.date = startOfDay
+        
+        updateDateViews()
+        updateNumberOfGuests()
+    }
+}
